5410937078bdd36f6da9385ddb94274541f0462b
Adrian Peter Krone <adrian.krone@tu-dortmund.de>
2023-09-25 20:15:30 +0200

    Bugfix in agepy.plot

--- src/agepy/plot/__init__.py
+++ src/agepy/plot/__init__.py
@@ -1,12 +1,18 @@
 import matplotlib.pyplot as plt
 
-available_styles = ["mpl_default", "age"]
-#available_styles.extend(plt.style.available)
+age_styles = ["age"]
+mpl_styles = ["default"]
+mpl_styles.append(plt.style.available)
 
 def use(styles):
     """
-    Function calling ``plt.style.use`` for easier access to the custom AGE
-    matplotlib style sheets.
+    Function calling ``plt.style.use`` for easier access to the custom 
+    AGE matplotlib style sheets.
+
+    Note
+    ----
+    All style rcParams are reset to the matplotlib default before 
+    loading the specified styles.
 
     Warning
     -------
@@ -15,24 +21,30 @@ def use(styles):
     Parameters
     ----------
     styles: str or list of string
-        Styles to be loaded using ``plt.style.use``. Available styles can 
-        be viewed by calling ``agepy.plot.available_styles``.
+        Styles to be loaded using ``plt.style.use``. Available styles 
+        can be viewed by calling ``agepy.plot.available_styles``.
 
     """
     load_styles = []
 
+    # Check if styles are available
     if isinstance(styles, list):
         for style in styles:
-            if style in available_styles:
+            if style in age_styles:
                 load_styles.append("agepy.plot." + style)
+            elif style in mpl_styles:
+                load_styles.append(style)
             else:
-                raise ValueError(style + " is not an available style. Available styles are: " + ", ".join(available_styles))
+                raise ValueError(style + " is not an available style.")
     elif isinstance(styles, str):
-        if styles in available_styles:
+        if styles in age_styles:
             load_styles.append("agepy.plot." + styles)
+        elif styles in mpl_styles:
+            load_styles.append(styles)
         else:
-            raise ValueError(styles + " is not an available style. Available styles are: " + ", ".join(available_styles))
+            raise ValueError(styles + " is not an available style.")
     else:
-        raise TypeError("Expected str or list of strings specifying available styles.")
+        raise TypeError("Expected str or list of strings specifying styles.")
     
-    plt.style.use(load_styles)
+    plt.style.use("default") # reset rcParams before applying the style
+    plt.style.use(load_styles) # apply the selected styles
