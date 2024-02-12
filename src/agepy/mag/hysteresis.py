494b2cf0257b2a5930c1f677740a7b36604ea527
Adrian Peter Krone <adrian.krone@physik.uni-kassel.de>
2024-02-12 10:59:30 +0100

    Removed unnecessary whitespaces (flake8)

--- src/agepy/mag/hysteresis.py
+++ src/agepy/mag/hysteresis.py
@@ -13,12 +13,12 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-def normalize(*magnetization: Sequence, 
+def normalize(*magnetization: Sequence,
               saturation_mean_percentage: float = 10) -> NDArray:
     """Normalizes hysteresis to the saturation region.
-    
+
     Magnetization is normalized to the averaged saturation over a given
-    range. The range over which to average is computed from a 
+    range. The range over which to average is computed from a
     percentage relative to the number of data points in one branch.
     The magnetization needs to be supplied either as one continous
     sequence containing both hysteresis branches or both branches as
@@ -52,28 +52,31 @@ def normalize(*magnetization: Sequence,
 
     if len(magnetization) > 2:
         raise ValueError("Too many input sequences.")
-    
+
     # calculate number of points over wich to average saturation
     # ceil ensures the presence of at least one data point
-    mean_range = np.ceil(len(branch_1) * saturation_mean_percentage / 100).astype(int)
+    mean_range = np.ceil(len(branch_1) * saturation_mean_percentage
+                         / 100).astype(int)
 
     # normalize
-    max_mag = np.mean(np.concatenate((branch_1[:mean_range], branch_2[len(branch_2) - mean_range:])))
-    min_mag = np.mean(np.concatenate((branch_2[:mean_range], branch_1[len(branch_2) - mean_range:])))
+    max_mag = np.mean(np.concatenate((branch_1[:mean_range],
+                                      branch_2[len(branch_2) - mean_range:])))
+    min_mag = np.mean(np.concatenate((branch_2[:mean_range],
+                                      branch_1[len(branch_2) - mean_range:])))
 
     branch_1 = 2 * (branch_1 - min_mag) / (max_mag - min_mag) - 1
     branch_2 = 2 * (branch_2 - min_mag) / (max_mag - min_mag) - 1
 
     if len(magnetization) == 1:
         return np.concatenate((branch_1, branch_2))
-    
+
     if len(magnetization) == 2:
         return np.array((branch_1, branch_2))
-    
+
 
 def shift_branch(*magnetization: Sequence, shift_value: float) -> NDArray:
     """Shifts hysteresis branches relative to each other.
-    
+
     The shift is applied to the second branch.
 
     Parameters
@@ -101,24 +104,24 @@ def shift_branch(*magnetization: Sequence, shift_value: float) -> NDArray:
 
     if len(magnetization) > 2:
         raise ValueError("Too many input sequences.")
-    
+
     # python built-in lists don't support += operator for maths
     branch_2 = np.array(branch_2)
     branch_2 = branch_2 + shift_value
 
     if len(magnetization) == 1:
         return np.concatenate((branch_1, branch_2))
-    
+
     if len(magnetization) == 2:
         return np.array((branch_1, branch_2))
-    
+
 
 def drift_correction(*hysteresis: ArrayLike, slope: float) -> NDArray:
     """Corrects thermal drift.
-    
+
     Thermal correction is applied by subtracting a linear function from
     the time ordered hysteresis loop.
-    Hysteresis data can be supplied either as one tuple containing the 
+    Hysteresis data can be supplied either as one tuple containing the
     magntic field and magnetization repectively or as multiple tuples
     containing the two hysteresis branches.
     This function is purely for subtracting a linear function in time
@@ -138,11 +141,11 @@ def drift_correction(*hysteresis: ArrayLike, slope: float) -> NDArray:
     NDArray
         The corrected magnetization values. If ``hysteresis`` is
         supplied as whole sequence , then a 1-D array with the same
-        length as the magnetization is returned. If ``hysteresis`` is 
+        length as the magnetization is returned. If ``hysteresis`` is
         two branches, a 2-D array is returned containing the
         two magnetization branches in the first axis.
 
-    
+
     Examples
     --------
     Viable example inputs.
@@ -173,28 +176,28 @@ def drift_correction(*hysteresis: ArrayLike, slope: float) -> NDArray:
 
     if len(hysteresis) > 2:
         raise ValueError("Too many input sequences.")
-    
+
     M_1 = M_1 + H_1 * slope
     offset = M_1[-1] - M_2[0] + H_2[0] * slope  # preserves line continuity
     M_2 = M_2 - H_2 * slope + offset
 
     if len(hysteresis) == 1:
         return np.concatenate((M_1, M_2))
-    
+
     if len(hysteresis) == 2:
         return np.array((M_1, M_2))
-    
 
-def linear_correction(*hysteresis: ArrayLike, 
+
+def linear_correction(*hysteresis: ArrayLike,
                       saturation_percentage: float = 10) -> NDArray:
     """Subtracts a linear fit to the saturation regions.
-    
+
     Linear fit is performed on the saturation region with respect to the
     magnetic field. The saturation region is defined by a percentage
     value relative to the number of data points of a single branch.
     A function ``f(x) = fit_slope * x`` is then subtracted from the
     magnetization.
-    Hysteresis data can be supplied either as one tuple containing the 
+    Hysteresis data can be supplied either as one tuple containing the
     magntic field and magnetization repectively or as multiple tuples
     containing the two hysteresis branches.
 
@@ -212,7 +215,7 @@ def linear_correction(*hysteresis: ArrayLike,
     NDArray
         The corrected magnetization values. If ``hysteresis`` is
         supplied as whole sequence , then a 1-D array with the same
-        length as the magnetization is returned. If ``hysteresis`` is 
+        length as the magnetization is returned. If ``hysteresis`` is
         two branches, a 2-D array is returned containing the
         two magnetization branches in the first axis.
 
@@ -248,7 +251,7 @@ def linear_correction(*hysteresis: ArrayLike,
 
     if len(hysteresis) > 2:
         raise ValueError("Too many input sequences.")
-    
+
     # ceil ensures at least one data point
     sat_range = np.ceil(len(H_1) * saturation_percentage / 100).astype(int)
     H1_sat = np.concatenate((H_1[:sat_range], H_2[len(H_1) - sat_range:]))
@@ -267,21 +270,21 @@ def linear_correction(*hysteresis: ArrayLike,
 
     if len(hysteresis) == 1:
         return np.concatenate((M_1, M_2))
-    
+
     if len(hysteresis) == 2:
         return np.array((M_1, M_2))
-    
 
-def faraday_correction(*hysteresis: ArrayLike, 
+
+def faraday_correction(*hysteresis: ArrayLike,
                        initial_guess: Sequence[float],
                        saturation_percentage: float = 10,
-                       log_level: int=logging.INFO) -> NDArray:
+                       log_level: int = logging.INFO) -> NDArray:
     """Performs a correction of the Faraday-effect via analytic
     optimization method.
 
     Note: It is recommended to use ``optimzed_verdet_correction``
     instead as it removes the additional guess of the cos amplitude.
-    
+
     Performs optimization of a functional describing the difference
     between saturation region and analytic expression of the Faraday-
     effect. The correction might need to be combined with other
@@ -290,7 +293,7 @@ def faraday_correction(*hysteresis: ArrayLike,
     Note, that the optimized functional considers the linearly
     corrected data, but the function itself does not subtract the linear
     part.
-    Hysteresis data can be supplied either as one tuple containing the 
+    Hysteresis data can be supplied either as one tuple containing the
     magntic field and magnetization repectively or as multiple tuples
     containing the two hysteresis branches.
 
@@ -314,7 +317,7 @@ def faraday_correction(*hysteresis: ArrayLike,
     NDArray
         The corrected magnetization values. If ``hysteresis`` is
         supplied as whole sequence , then a 1-D array with the same
-        length as the magnetization is returned. If ``hysteresis`` is 
+        length as the magnetization is returned. If ``hysteresis`` is
         two branches, a 2-D array is returned containing the
         two magnetization branches in the first axis.
 
@@ -322,21 +325,21 @@ def faraday_correction(*hysteresis: ArrayLike,
     --------
     Viable example inputs.
 
-    >>> faraday_correction((H, M), initial_guess=(0.01, 5, 100), 
+    >>> faraday_correction((H, M), initial_guess=(0.01, 5, 100),
                             saturation_percentage=10)
     array([<M-values>])
 
-    >>> faraday_correction((H1, M1), (H2, M2), initial_guess=(0.01, 5, 100), 
+    >>> faraday_correction((H1, M1), (H2, M2), initial_guess=(0.01, 5, 100),
                             saturation_percentage=10)
     array([[<M1-values>], [<M2-values>]])
 
     >>> hyst = array([H, M])
-    >>> faraday_correction(hyst, initial_guess=(0.01, 5, 100), 
+    >>> faraday_correction(hyst, initial_guess=(0.01, 5, 100),
                             saturation_percentage=10)
     array([<M-values>])
 
     >>> hyst = array([[H1, M1], [H2, M2]])
-    >>> faraday_correction(*hyst, initial_guess=(0.01, 5, 100), 
+    >>> faraday_correction(*hyst, initial_guess=(0.01, 5, 100),
                             saturation_percentage=10)
     array([[<M1-values>], [<M2-values>]])
     """
@@ -351,13 +354,13 @@ def faraday_correction(*hysteresis: ArrayLike,
 
     if len(hysteresis) > 2:
         raise ValueError("Too many input sequences.")
-    
+
     # ceil ensures at least one data point
     sat_range = np.ceil(len(H) / 2 * saturation_percentage / 100).astype(int)
 
     def cos_sq(x, A, v, s):
         """Parameterized cos^2 function.
-        
+
         Parameters
         ----------
         x: float
@@ -374,35 +377,36 @@ def faraday_correction(*hysteresis: ArrayLike,
 
     def functional(x):
         """Functional to optimize.
-        
+
         Assumes the ideal correction is reached, when the saturation
         flattens out.
         """
 
         cos_squared = cos_sq(H, *x)
-        corrected = linear_correction((H, M - cos_squared), 
-                                saturation_percentage=saturation_percentage)
+        corrected = linear_correction(
+            (H, M - cos_squared), saturation_percentage=saturation_percentage)
         derivative = np.gradient(corrected)
         branch_1, branch_2 = np.split(derivative, 2)
         saturation = np.concatenate((
-            branch_1[:sat_range], branch_1[len(branch_1) - sat_range:], 
+            branch_1[:sat_range], branch_1[len(branch_1) - sat_range:],
             branch_2[:sat_range], branch_2[len(branch_2) - sat_range:]))
-        
+
         return np.mean(np.abs(saturation))
 
     # optimize the functional
     minimum = minimize(functional, x0=initial_guess)
-    logger.log(log_level, f"Computed minimum for faraday correction:\n{minimum}")
+    logger.log(log_level,
+               f"Computed minimum for faraday correction:\n{minimum}")
     M_corrected = M - cos_sq(H, *(minimum.x))
 
     if len(hysteresis) == 1:
         return M_corrected
-    
+
     if len(hysteresis) == 2:
         return np.array(np.split(M_corrected, 2))
-    
 
-def optimized_faraday_correction(*hysteresis: ArrayLike, 
+
+def optimized_faraday_correction(*hysteresis: ArrayLike,
                                  verdet_guess: float,
                                  left_shift_guess: float,
                                  bracket: Sequence[float] = None,
@@ -411,7 +415,7 @@ def optimized_faraday_correction(*hysteresis: ArrayLike,
                                  log_level: int = logging.INFO) -> NDArray:
     """Performs a correction of the Faraday-effect via analytic
     optimization method.
-    
+
     Performs optimization of a functional describing the difference
     between saturation region and analytic expression of the Faraday-
     effect. The correction might need to be combined with other
@@ -420,7 +424,7 @@ def optimized_faraday_correction(*hysteresis: ArrayLike,
     Note, that the optimized functional considers the linearly
     corrected data, but the function itself does not subtract the linear
     part.
-    Hysteresis data can be supplied either as one tuple containing the 
+    Hysteresis data can be supplied either as one tuple containing the
     magntic field and magnetization repectively or as multiple tuples
     containing the two hysteresis branches.
 
@@ -453,7 +457,7 @@ def optimized_faraday_correction(*hysteresis: ArrayLike,
     NDArray
         The corrected magnetization values. If ``hysteresis`` is
         supplied as whole sequence , then a 1-D array with the same
-        length as the magnetization is returned. If ``hysteresis`` is 
+        length as the magnetization is returned. If ``hysteresis`` is
         two branches, a 2-D array is returned containing the
         two magnetization branches in the first axis.
 
@@ -502,22 +506,22 @@ def optimized_faraday_correction(*hysteresis: ArrayLike,
     a clean hysteresis this last step is therefore necessary.
 
     Viable example inputs.
-    >>> faraday_correction((H, M), verdet_guess=0.01, left_shift_guess=5,  
+    >>> faraday_correction((H, M), verdet_guess=0.01, left_shift_guess=5,
                             saturation_percentage=10)
     array([<M-values>])
 
-    >>> faraday_correction((H1, M1), (H2, M2), verdet_guess=0.01, 
-                            left_shift_guess=5,  
+    >>> faraday_correction((H1, M1), (H2, M2), verdet_guess=0.01,
+                            left_shift_guess=5,
                             saturation_percentage=10)
     array([[<M1-values>], [<M2-values>]])
 
     >>> hyst = array([H, M])
-    >>> faraday_correction(hyst, verdet_guess=0.01, left_shift_guess=5,  
+    >>> faraday_correction(hyst, verdet_guess=0.01, left_shift_guess=5,
                             saturation_percentage=10)
     array([<M-values>])
 
     >>> hyst = array([[H1, M1], [H2, M2]])
-    >>> faraday_correction(*hyst, verdet_guess=0.01, left_shift_guess=5,  
+    >>> faraday_correction(*hyst, verdet_guess=0.01, left_shift_guess=5,
                             saturation_percentage=10)
     array([[<M1-values>], [<M2-values>]])
     """
@@ -532,28 +536,28 @@ def optimized_faraday_correction(*hysteresis: ArrayLike,
 
     if len(hysteresis) > 2:
         raise ValueError("Too many input sequences.")
-    
+
     # ceil ensures at least one data point
     sat_range = np.ceil(len(H)/2 * saturation_percentage / 100).astype(int)
 
     def functional(scale):
         """Wrapping functional for determining the correct amplitude.
-        
+
         Assumes the ideal correction is reached, when the saturation
         flattens out.
         """
 
-        corrected = faraday_correction((H, M), initial_guess=(scale, 
-                                                              verdet_guess, 
-                                                              left_shift_guess),
-                                        saturation_percentage=saturation_percentage,
-                                        log_level=logging.DEBUG)
+        corrected = faraday_correction(
+            (H, M), initial_guess=(scale, verdet_guess, left_shift_guess),
+            saturation_percentage=saturation_percentage,
+            log_level=logging.DEBUG
+        )
         derivative = np.gradient(corrected)
         branch_1, branch_2 = np.split(derivative, 2)
         saturation = np.concatenate((
-            branch_1[:sat_range], branch_1[len(branch_1) - sat_range:], 
+            branch_1[:sat_range], branch_1[len(branch_1) - sat_range:],
             branch_2[:sat_range], branch_2[len(branch_2) - sat_range:]))
-        
+
         return np.mean(np.abs(saturation))
 
     # optimize the functional
@@ -563,15 +567,13 @@ def optimized_faraday_correction(*hysteresis: ArrayLike,
         minimum = minimize_scalar(functional, bounds=bounds, method="bounded")
 
     logger.log(log_level, f"Computed minimum for amplitude guess:\n{minimum}")
-    M_corrected = faraday_correction((H, M),
-                                     initial_guess=(minimum.x,
-                                                    verdet_guess,
-                                                    left_shift_guess),
-                                     saturation_percentage=saturation_percentage,
-                                     log_level=log_level)
+    M_corrected = faraday_correction(
+        (H, M), initial_guess=(minimum.x, verdet_guess, left_shift_guess),
+        saturation_percentage=saturation_percentage, log_level=log_level
+    )
 
     if len(hysteresis) == 1:
         return M_corrected
-    
+
     if len(hysteresis) == 2:
         return np.array(np.split(M_corrected, 2))
